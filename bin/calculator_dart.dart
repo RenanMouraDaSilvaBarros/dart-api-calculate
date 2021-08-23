cb6d8c0f7663f2c600b88e5928fe222f72973cbb
Renan Moura <naneraruom@gmail.com>
2021-08-23 10:12:50 -0300

    init project

---
+++ bin/calculator_dart.dart
@@ -0,0 +1,5 @@
+import 'package:calculator_dart/calculator_dart.dart' as calculator_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${calculator_dart.calculate()}!');
+}
---
+++ test/calculator_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:calculator_dart/calculator_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
