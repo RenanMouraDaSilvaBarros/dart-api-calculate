776a4b8b09f294f459313d48452909eecd3cff47
Renan Moura <naneraruom@gmail.com>
2021-08-23 12:38:22 -0300

    port

--- bin/calculator_dart.dart
+++ bin/calculator_dart.dart
@@ -2,7 +2,7 @@ import 'package:calculator_dart/math_controllers.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
 
 void main() async {
-  const PORT =  7782;
+  const PORT =  3000;
   final home = MathController();
   final server = await shelf_io.serve(home.math(), '0.0.0.0', PORT);
   print('☀️ Server running on localhost:${server.port} ☀️');
